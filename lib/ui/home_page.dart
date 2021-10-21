55d1ba8e6b31a5b22cffab7331582f7005a651f8
Gabriel Menoi <ga.gabrielmenoi@gmail.com>
2021-10-21 09:56:27 -0300

    terceiro commit

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
-
 import 'package:buscador_gifs/ui/gif_page.dart';
 import 'package:flutter/material.dart';
-
 import 'package:http/http.dart' as http;
 import 'package:share/share.dart';
 import 'package:transparent_image/transparent_image.dart';
@@ -14,7 +12,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   String _search;
 
   int _offset = 0;
@@ -22,20 +19,21 @@ class _HomePageState extends State<HomePage> {
   Future<Map> _getGifs() async {
     http.Response response;
 
-    if(_search == null || _search.isEmpty)
-      response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=wusbFwUExpkztfjeMr3QRimPUc4kd1J9&limit=20&rating=G");
+    if (_search == null || _search.isEmpty)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=wusbFwUExpkztfjeMr3QRimPUc4kd1J9&limit=20&rating=G");
     else
-      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=wusbFwUExpkztfjeMr3QRimPUc4kd1J9&q=$_search&limit=19&offset=$_offset&rating=G&lang=en");
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=wusbFwUExpkztfjeMr3QRimPUc4kd1J9&q=$_search&limit=19&offset=$_offset&rating=G&lang=en");
 
     return json.decode(response.body);
   }
-
 
   @override
   void initState() {
     super.initState();
 
-    _getGifs().then((map){
+    _getGifs().then((map) {
       print(map);
     });
   }
@@ -45,7 +43,8 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Image.network("https://developers.giphy.com/static/img/dev-logo-lg.gif"),
+        title: Image.network(
+            "https://developers.giphy.com/static/img/dev-logo-lg.gif"),
         centerTitle: true,
       ),
       backgroundColor: Colors.black,
@@ -57,11 +56,10 @@ class _HomePageState extends State<HomePage> {
               decoration: InputDecoration(
                   labelText: "Pesquise Aqui!",
                   labelStyle: TextStyle(color: Colors.white),
-                  border: OutlineInputBorder()
-              ),
+                  border: OutlineInputBorder()),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
-              onSubmitted: (text){
+              onSubmitted: (text) {
                 setState(() {
                   _search = text;
                   _offset = 0;
@@ -72,8 +70,8 @@ class _HomePageState extends State<HomePage> {
           Expanded(
             child: FutureBuilder(
                 future: _getGifs(),
-                builder: (context, snapshot){
-                  switch(snapshot.connectionState){
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
                     case ConnectionState.waiting:
                     case ConnectionState.none:
                       return Container(
@@ -81,55 +79,58 @@ class _HomePageState extends State<HomePage> {
                         height: 200.0,
                         alignment: Alignment.center,
                         child: CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
                           strokeWidth: 5.0,
                         ),
                       );
                     default:
-                      if(snapshot.hasError) return Container();
-                      else return _createGifTable(context, snapshot);
+                      if (snapshot.hasError)
+                        return Container();
+                      else
+                        return _createGifTable(context, snapshot);
                   }
-                }
-            ),
+                }),
           ),
         ],
       ),
     );
   }
 
-  int _getCount(List data){
-    if(_search == null){
+  int _getCount(List data) {
+    if (_search == null) {
       return data.length;
     } else {
       return data.length + 1;
     }
   }
 
-  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
     return GridView.builder(
         padding: EdgeInsets.all(10.0),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0
-        ),
+            crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
         itemCount: _getCount(snapshot.data["data"]),
-        itemBuilder: (context, index){
-          if(_search == null || index < snapshot.data["data"].length)
+        itemBuilder: (context, index) {
+          if (_search == null || index < snapshot.data["data"].length)
             return GestureDetector(
               child: FadeInImage.memoryNetwork(
-                  placeholder: kTransparentImage,
-                  image: snapshot.data["data"][index]["images"]["fixed_height"]["url"],
-                  height: 300.0,
-                  fit: BoxFit.cover,
+                placeholder: kTransparentImage,
+                image: snapshot.data["data"][index]["images"]["fixed_height"]
+                    ["url"],
+                height: 300.0,
+                fit: BoxFit.cover,
               ),
-              onTap: (){
-                Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => GifPage(snapshot.data["data"][index]))
-                );
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            GifPage(snapshot.data["data"][index])));
               },
-              onLongPress: (){
-                Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
+              onLongPress: () {
+                Share.share(snapshot.data["data"][index]["images"]
+                    ["fixed_height"]["url"]);
               },
             );
           else
@@ -138,20 +139,24 @@ class _HomePageState extends State<HomePage> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Icon(Icons.add, color: Colors.white, size: 70.0,),
-                    Text("Carregar mais...",
-                      style: TextStyle(color: Colors.white, fontSize: 22.0),)
+                    Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 70.0,
+                    ),
+                    Text(
+                      "Carregar mais...",
+                      style: TextStyle(color: Colors.white, fontSize: 22.0),
+                    )
                   ],
                 ),
-                onTap: (){
+                onTap: () {
                   setState(() {
                     _offset += 19;
                   });
                 },
               ),
             );
-        }
-    );
+        });
   }
-
 }
