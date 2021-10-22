98caed28597853db49e741ad91d8de84d7226060
Gabriel Menoi <ga.gabrielmenoi@gmail.com>
2021-10-22 09:12:27 -0300

    quarto commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,8 @@ import 'package:buscador_gifs/ui/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: HomePage(),
     theme: ThemeData(hintColor: Colors.white),
   ));
 }
-
-
